4bced251b9457b495999026cfb05eeafa117d3ba
Simone Fantini <sfantini@bsdsoftware.it>
2014-02-18 18:10:44 +0100

    Aggiunta animazione all'aggiunta della window a schermo.

--- RKiOS7Loading/RKiOS7Loading+Window.m
+++ RKiOS7Loading/RKiOS7Loading+Window.m
@@ -18,23 +18,31 @@ static RKiOS7Loading *_hudInstance;
 		if (_hudInstance)
 			return;
 		
-		_hudWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+		_hudWindow = [[UIWindow alloc] initWithFrame:(CGRect){0, [UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size}];
 		_hudWindow.windowLevel = UIWindowLevelStatusBar;
-		_hudWindow.backgroundColor = [UIColor colorWithWhite:0.8 alpha:0.6];
+		_hudWindow.backgroundColor = [UIColor colorWithWhite:0.7 alpha:0.6];
 		[_hudWindow makeKeyAndVisible];
 		_hudInstance = [RKiOS7Loading showHUDAddedTo:_hudWindow animated:YES];
 		[_hudInstance startSpinProgressBackgroundLayer];
+		
+		[UIView animateWithDuration:0.3 animations:^{
+			_hudWindow.frame = [UIScreen mainScreen].bounds;
+		}];
 	}
 }
 
 + (void)hideHUD {
 	@synchronized(_hudInstance) {
 		if (_hudInstance) {
-			[_hudInstance stopSpinProgressBackgroundLayer];
-			_hudInstance = nil;
-			[RKiOS7Loading hideHUDForView:_hudWindow animated:NO];
-			_hudWindow.hidden = YES;
-			_hudWindow = nil;
+			[UIView animateWithDuration:0.3 animations:^{
+				_hudWindow.frame = (CGRect){0, [UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size};
+			} completion:^(BOOL finished) {
+				[_hudInstance stopSpinProgressBackgroundLayer];
+				_hudInstance = nil;
+				[RKiOS7Loading hideHUDForView:_hudWindow animated:NO];
+				_hudWindow.hidden = YES;
+				_hudWindow = nil;
+			}];
 		}
 	}
 }
